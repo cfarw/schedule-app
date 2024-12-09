42e886760aacf32e100ee8b96bc09f80e3d2ba25
El_Rovi Soft <el.rovisoft@gmail.com>
2024-12-09 17:16:12 +0300

    I hope that's a final commit

---
+++ Database.cs
@@ -0,0 +1,69 @@
+﻿namespace schedule_app {
+	internal class EventPole(uint id, DateTime date, string event_name) {
+		public uint member_id = id;
+		public DateTime date = date;
+		public string event_name = event_name;
+	}
+
+	class Database {
+		private readonly Dictionary<string, uint> _members_by_name;
+		private readonly Dictionary<uint, string> _members_by_id;
+		private List<EventPole> _events;
+
+		public Database(string[]? members_collection) {
+			_members_by_name = new();
+			_members_by_id = new();
+			_events = new();
+
+			if (members_collection is not null)
+				init_values(members_collection);
+		}
+
+		public void init_values(string[] members_collection) {
+			_members_by_name.Clear();
+			foreach (var member in members_collection) {
+				_members_by_name.Add(member, (uint) _members_by_name.Count);
+				_members_by_id.Add((uint) _members_by_id.Count, member);
+			}
+		}
+
+		public void add_event(string member_name, DateTime date, string event_name) {
+			_events.Add(new EventPole(_members_by_name[member_name], date, event_name));
+		}
+
+		public List<EventPole> get_events(string member_name) {
+			List<EventPole> result = new();
+			if (member_name != "@all") {
+                foreach (EventPole pole in _events) {
+                    if (pole.member_id == _members_by_name[member_name])
+                        result.Add(pole);
+                }
+            } else
+                result = _events;
+            return result;
+		}
+		public List<EventPole> get_events(string member_name, DateTime date) {
+            List<EventPole> result = new();
+            if (member_name != "@all") {
+                foreach (EventPole pole in _events) {
+                    if (pole.member_id == _members_by_name[member_name] && check_dates(pole.date, date))
+                        result.Add(pole);
+                }
+            } else {
+                foreach (EventPole pole in _events) {
+                    if (check_dates(pole.date, date))
+                        result.Add(pole);
+                }
+            }
+            return result;
+        }
+
+		public string get_member_by_id(uint id) {
+			return _members_by_id[id];
+		}
+
+		private bool check_dates(DateTime lhs, DateTime rhs) {
+			return lhs.Year == rhs.Year && lhs.Month == rhs.Month && lhs.Day == rhs.Day;
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace schedule_app {
+	public partial class MainWindow {
+		private Database _db;
+		private DateTime _default_date = DateTime.Today;
+
+		public MainWindow() {
+			InitializeComponent();
+
+			_db = new Database(null);
+
+			init_names([
+				"@all",
+				"@everyone",
+				"S.Coups",
+				"Jeonghan",
+				"Joshua",
+				"Jun",
+				"Hoshi",
+				"Wonwoo",
+				"Woozi",
+				"The8",
+				"Mingyu",
+				"DK",
+				"Seungkwan",
+				"Vernon",
+				"Dino"
+			]);
+			init_events();
+
+			date_picker.SelectedDate = _default_date;
+		}
+
+		private void init_names(string[] names) {
+			_db.init_values(names);
+			members_combo_box.ItemsSource = names;
+			members_combo_box.SelectedIndex = 0;
+		}
+		private void init_events() {
+			_db.add_event("@everyone", new DateTime(2025, 4, 5, 12, 30, 0), "Fanmeeting in Seoul");
+		}
+
+		private void filter_button_clicked_event(object sender, RoutedEventArgs e) {
+            bool status = check_box.IsChecked ?? false;
+			var events = status ?
+				_db.get_events((string) members_combo_box.SelectedItem, date_picker.SelectedDate ?? _default_date) :
+                _db.get_events((string) members_combo_box.SelectedItem);
+
+			list_box.Items.Clear();
+			foreach(var it in events)
+				list_box.Items.Add(_db.get_member_by_id(it.member_id) + " " + it.date.ToString() + " " + it.event_name);
+		}
+	}
+}
